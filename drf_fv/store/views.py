d15e7863f9a30608f6ac84855a0761bcac93e607
abolfazl <abolfazlrasouli95@gmail.com>
2024-04-27 07:46:39 +0330

    send post request and is_valid methd

--- drf_fv/store/views.py
+++ drf_fv/store/views.py
@@ -51,17 +51,18 @@ from .serializers import ProductSerializer, CategorySerializer
 
 # ===========================/___5___/=================================
 @api_view()
-def product_list(request):
-    product = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(product, many=True, context={'request': request})
-    return Response(serializer.data)
+# def product_list(request):
+#     product = Product.objects.select_related('category').all()
+#     serializer = ProductSerializer(product, many=True, context={'request': request})
+#     return Response(serializer.data)
+#
+#
+# @api_view()
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
+#     serializer = ProductSerializer(product, context={'request': request})
+#     return Response(serializer.data)
 
-
-@api_view()
-def product_detail(request, pk):
-    product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
-    serializer = ProductSerializer(product, context={'request': request})
-    return Response(serializer.data)
 
 # ===========================/___6___/=================================
 @api_view()
@@ -70,3 +71,45 @@ def category_detail(request, pk):
     serializer = CategorySerializer(category, context={'request': request})
     return Response(serializer.data)
 
+
+# ===========================/___7___/=================================
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        product = Product.objects.select_related('category').all()
+        serializer = ProductSerializer(product, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.validated_data
+        #     return Response('ok')
+        # else:
+        #     return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        if product.order_items.count() > 0:
+            return Response({
+                'error':'There is some order item including this product . please remove them first.'},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
