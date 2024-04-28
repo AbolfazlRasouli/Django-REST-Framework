ea364be5f952fe4bd22bf6179b42a8a098a002b1
abolfazl <abolfazlrasouli95@gmail.com>
2024-04-28 08:17:28 +0330

    serializer Nested Router for comments

--- drf_cbv/store/urls.py
+++ drf_cbv/store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
-# from rest_framework_nested import routers
+from rest_framework_nested import routers
 
 
 # urlpatterns = [
@@ -23,13 +23,29 @@ from . import views
 # ]
 
 # ====================================  DefaultRouter() =========================================
-router = DefaultRouter()
+# router = DefaultRouter()
+# router.register('products', views.ProductViewSet, basename='product')
+# router.register('categories', views.CategoryViewSet, basename='category')
+#
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
+
+# ====================================  nested routers =========================================
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('categories', views.CategoryViewSet, basename='category')
 
-urlpatterns = [
-    path('', include(router.urls))
-]
+product_routers = routers.NestedDefaultRouter(router, 'products', lookup='product')
 
+product_routers.register('comments', views.CommentViewSet, basename='product_comment')
+
+# urlpatterns = router.urls + product_routers.urls
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+] + product_routers.urls
 
 
